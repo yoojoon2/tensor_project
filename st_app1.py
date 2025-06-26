f1792e0887c7410c926ecfd77ed40af82b086608
yoojoon2 <aladin409@naver.com>
2025-06-26 15:27:05 +0900

    0626 15시

--- st_app1.py
+++ st_app1.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title='Hello'
+    page_title='Hello'    
 )
 
-st.write("Hello, Streamlit!")
+st.write('### 안녕하세요 반갑습니다.!!!!!!!!!')
 
-st.sidebar.success('데모선택')
+st.sidebar.success('데모 선택')
 
 st.markdown(
 """
@@ -17,3 +17,13 @@ Streamlit은 머신 러닝 및 데이터 과학 프로젝트를 위해 특별히
 - [커뮤니티 포럼](https://discuss.streamlit.io)에서 질문하기
 """
 )
+
+st.code("a = 1234")
+
+st.latex("\int a x^2 \,dx")
+
+name = st.text_input("이름")
+
+print(name)
+
+st.write(name)
